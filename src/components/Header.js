dffcd87a0ad9c24e27a33ac37bc71d1c0c0365a3
Mariia Zhvanko <mzhvanko@amtoss.com>
2023-02-20 19:10:42 +0200

    tailwind header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "../styles/Header.module.scss"
 export default function Header() {
     return (
         <header className='flex justify-between bg-white px-[10.5%] py-[8px] font-inter font-normal text-lg'>
@@ -9,7 +8,7 @@ export default function Header() {
                     <Image src='/pin_icon.svg' width={13.97} height={13.97} alt='pin'></Image>
                     <span className='ml-[8.72px]'>Pin the unit</span></div>
             </div>
-            <div className='flex'>
+            <div className='flex items-center'>
                 <button className='mr-5 header-button'><Image src='/add_member_icon.png' width={18.33} height={13.33} alt='add member'></Image></button>
                 <Image className='member' src='/member1.png' width={32} height={32} alt='member'></Image>
                 <Image className='member' src='/member2.png' width={32} height={32} alt='member'></Image>
