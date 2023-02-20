b47a16db8d1bdab082266da213a6a82ac50f45c8
Mariia Zhvanko <mzhvanko@amtoss.com>
2023-02-20 04:11:09 +0200

    closing the preview mode message

--- src/components/PreviewModeMessage.js
+++ src/components/PreviewModeMessage.js
@@ -1,13 +1,14 @@
 import styles from "../styles/PreviewModeMessage.module.scss"
 import Image from "next/image";
+import {useState} from "react";
 export default function PreviewModeMessage() {
 
-    // const [isOpen, setOpen] = useState(false)
-    //
-    // return (
-    //     <div className={styles.wrapper} style={isOpen ? {display: "block"} : {display: "none"}}>
-    //         <p>Preview mode</p>
-    //         <button onClick={() => {isOpen = false}}><Image src='/close_icon.svg' width={12} height={12} alt='close'></Image></button>
-    //     </div>
-    // )
+    const [isShown, setShown] = useState(true)
+
+    return (
+        <div className={styles.wrapper} style={isShown ? {display: "flex"} : {display: "none"}}>
+            <p>Preview mode</p>
+            <button onClick={() => {setShown(!isShown)}}><Image src='/close_icon.svg' width={12} height={12} alt='close'></Image></button>
+        </div>
+    )
 }
